6abf75858bc953065efe89ebda42a0c010e16d0a
Libo Su <sulibozju@gmail.com>
2022-08-13 22:00:08 +0200

    Site updated at: 2022-08-13 20:00:08 UTC

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220813.215751';
+const cacheName = 'chirpy-20220813.220001';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
